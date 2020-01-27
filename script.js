39be2b470ef144133038fd441061863589317ef6
Cameron Scott <camscottgreen@hotmail.com>
2020-01-27 12:05:38 +1000

    Implement basic + table rendering

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ function Book(title, author, pages, read) {
         }
     }
 }
+
+function renderTable() {
+    let table = document.querySelector("table");
+    let new_tbody = document.createElement('tbody');
+    let index = 0;
+    myLibrary.forEach(book => {
+        let row = new_tbody.insertRow();
+        let title = row.insertCell();
+        title.innerHTML = book.title;
+        let author = row.insertCell();
+        author.innerHTML = book.author;
+        let pages = row.insertCell();
+        pages.innerHTML = book.pages;
+        let read = row.insertCell();
+        read.innerHTML = book.readString();
+        index++;
+    });
+    
+    table.replaceChild(new_tbody, table.getElementsByTagName('tbody')[0]);
+}
+
+
+function render() {
+    let body = document.querySelector("body");
+    myLibrary.forEach(book => {
+        let elem = document.createElement('p');
+        elem.textContent = book.info();
+        body.appendChild(elem);
+    });
+}
+
